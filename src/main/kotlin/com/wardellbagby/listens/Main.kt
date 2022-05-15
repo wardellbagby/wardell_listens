8849c5ffa4b1a65708e9d39e5c6901d88ccf8ca0
Wardell Bagby <7834478+wardellbagby@users.noreply.github.com>
2022-05-15 10:16:11 -0700

    Dynamically choose tweet template to make sure it's under the max tweet length

--- src/main/kotlin/com/wardellbagby/listens/Main.kt
+++ src/main/kotlin/com/wardellbagby/listens/Main.kt
@@ -6,14 +6,70 @@ import kotlin.io.path.writeText
 import kotlin.time.Duration.Companion.days
 
 private const val ignoredTracksFile = "ignored.txt"
-private fun SuggestedTrack.asTweet(): String {
-  return """
-    This week's song suggestion is:
-    
-    $name by $artist
+private const val URL_TEMPLATE = "{URL}"
+private const val TWEET_MAX_LENGTH = 280
+private const val TWITTER_URL_LENGTH = 23
 
-    $spotifyUrl"""
+private val SuggestedTrack.extendedTweetTemplate: String
+  get() = """
+  This week's song suggestion is:
+  
+  $name by $artist
+
+  $URL_TEMPLATE
+  
+  #MusicMonday
+  """
     .trimIndent()
+
+private val SuggestedTrack.standardTweetTemplate: String
+  get() = """
+  This week's song suggestion is:
+  
+  $name
+
+  $URL_TEMPLATE
+  
+  #MusicMonday
+  """
+    .trimIndent()
+
+private const val shortTweetTemplate: String = """
+    This week's song suggestion is:
+
+    $URL_TEMPLATE
+    
+    #MusicMonday
+    """
+
+/**
+ * Replaces instances of [URL_TEMPLATE] with [replacement].
+ *
+ * By default, [replacement] is a string of length [TWITTER_URL_LENGTH], which enables accurately
+ * knowing the length of a tweet before we post it.
+ */
+private fun String.replaceUrlTemplate(
+  replacement: String = "A".repeat(TWITTER_URL_LENGTH)
+): String = replace(
+  URL_TEMPLATE,
+  replacement
+)
+
+/**
+ * Convert this [SuggestedTrack] into a tweet. The format of the Tweet depends on how long the title
+ * and artist of this [SuggestedTrack] is. This will choose the longest possible tweet that doesn't
+ * go over [TWEET_MAX_LENGTH].
+ */
+private fun SuggestedTrack.asTweet(): String {
+  return sequence {
+    yield(extendedTweetTemplate to extendedTweetTemplate.replaceUrlTemplate())
+    yield(standardTweetTemplate to standardTweetTemplate.replaceUrlTemplate())
+    yield(shortTweetTemplate to shortTweetTemplate.replaceUrlTemplate())
+  }
+    .first { (_, templateWithMockUrl) ->
+      templateWithMockUrl.length <= TWEET_MAX_LENGTH
+    }
+    .let { (template, _) -> template.replaceUrlTemplate(spotifyUrl) }
 }
 
 suspend fun main() {
